52b1ae353edfcc2a50ca951d7cee4d3a1d0e47c9
Guven Ayvazoglu <gayvazoglu.29@gmail.com>
2022-01-19 14:00:17 +0300

    updated

--- src/main/java/com/todolist/butan/ButanApplication.java
+++ src/main/java/com/todolist/butan/ButanApplication.java
@@ -6,9 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ButanApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ButanApplication.class, args);
     }
-
 }
